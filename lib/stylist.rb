0e6e80a0a0a1676e474461183fe3d8d813af9130
joshua <jrinard@hotmail.com>
2016-12-09 08:36:38 -0800

    unit spec for name and save

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -11,10 +11,15 @@ class Stylist
       stylists = []
       returned_stylists.each() do |stylist|
         name = stylist.fetch("name")
-        id = stylist.fetch("id").to_i
+        id = stylist.fetch("id").to_i()
         stylists.push(Stylist.new({:name => name, :id => id}))
       end
       stylists
+    end
+
+    define_method(:save) do
+      result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+      @id = result.first().fetch("id").to_i()
     end
 
 
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -6,4 +6,25 @@ describe(Stylist) do
       expect(Stylist.all()).to(eq([]))
     end
   end
+
+
+  describe("#name") do
+    it("tells you the name") do
+      stylist = Stylist.new({:name => "Quin", :id => nil})
+      expect(stylist.name()).to(eq("Quin"))
+    end
+  end
+
+  describe("#id") do
+    it("sets the ID when you save it") do
+      stylist = Stylist.new({:name => "Quin", :id => nil})
+      stylist.save()
+      expect(stylist.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+
+
+
+
 end
