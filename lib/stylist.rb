07fb4cc9b991d2a66e64fcee26cdc7b635a3eced
joshua <jrinard@hotmail.com>
2016-12-09 09:27:59 -0800

    finish unit spec and methods for stylist

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -26,5 +26,49 @@ class Stylist
       self.name().==(another_stylist.name()).&(self.id().==(another_stylist.id()))
     end
 
+    define_singleton_method(:find) do |id|
+      found_stylist = nil
+      Stylist.all().each() do |stylist|
+        if stylist.id().==(id)
+          found_stylist = list
+        end
+      end
+      found_stylist
+    end
+
+    # define_method(:clients) do
+    #   stylist_clients = []
+    #   clients = DB.exec("SELECT * FROM clients WHERE stylist_id = #{self.id()}")
+    #   clients.each() do |client|
+    #     name = client.fetch("name")
+    #     stylist_id = client.fetch("stylist_id").to_i
+    #     stylist_clients.push(Client.new({:name => name, :stylist_id => stylist_id}))
+    #   end
+    #   stylist_clients
+    # end
+
+  #update stylist
+    define_method(:update) do |attributes|
+      @name = attributes.fetch(:name)
+      @id = self.id()
+      DB.exec("UPDATE stylists SET name = '#{@name}' WHERE id = #{@id};")
+    end
+
+  #delete stylist
+    define_method(:delete) do
+      DB.exec("DELETE FROM stylists WHERE id = #{self.id()};")
+      DB.exec("DELETE FROM clients WHERE stylist_id = #{self.id()};")
+    end
+
+  # #delete entire table
+  #   define_singleton_method(:clear) do
+  #     DB.exec("TRUNCATE TABLE clients")
+  #   end
+
+
+
+
+
+
 
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -39,6 +39,62 @@ describe(Stylist) do
     end
   end
 
+  describe(".find") do
+    it("returns a list by its ID") do
+      test_stylist = Stylist.new({:name => "Quin", :id => nil})
+      test_stylist.save()
+      test_stylist = Stylist.new({:name => "Quin", :id => nil})
+      test_stylist.save()
+    end
+  end
+
+  #
+  # describe("#clients") do
+  #   it("returns an array of clients for that sylist") do
+  #     test_stylist = Stylist.new({:name => "Quin", :id => nil})
+  #     test_stylist.save()
+  #     test_client = Client.new({:name => "Jackson", :id => nil})
+  #     test_client.save()
+  #     test_client2 = Client.new({:name => "Garrett", :id => nil})
+  #     test_client2.save()
+  #     expect(test_stylist.clients()).to(eq([test_client, test_client2]))
+  #   end
+  # end
+
+#update a stylist
+  describe("#update") do
+    it("lets you update stylists in the database") do
+      test_stylist = Stylist.new({:name => "Quin", :id => nil})
+      test_stylist.save()
+      test_stylist.update({:name => 'Lillian'})
+      expect(test_stylist.name()).to(eq("Lillian"))
+    end
+  end
+
+#delete a stylist
+  describe("#delete") do
+    it("lets you delete a stylist from the database") do
+      test_stylist = Stylist.new({:name => "Quin", :id => nil})
+      test_stylist.save()
+      test_stylist2 = Stylist.new({:name => "Jackson", :id => nil})
+      test_stylist2.save()
+      test_stylist.delete()
+      expect(Stylist.all()).to(eq([test_stylist2]))
+    end
+    # it("deletes a stylist's clients from the database")do
+    #   test_stylist = Stylist.new({:name => "Quin", :id => nil})
+    #   test_stylist.save()
+    #   test_client = Client.new({:name => "Jackson", :id => nil})
+    #   test_client.save()
+    #   test_stylist.delete()
+    #   expect(Stylist.all()).to(eq([]))
+    # end
+  end
+
+
+
+
+
 
 
 
